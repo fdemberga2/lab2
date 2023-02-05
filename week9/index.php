a690d6672ba68199855e93351947f437b468fd3a
Felix Miguel Embarga <fdemberga2@student.apc.edu.ph>
2023-02-05 20:41:45 +0800

    Addition of week9 folder, Changes to file extensions, and creation of Validation.php

---
+++ week9/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Wanna know me?</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div id="navbar" id="top">
+        <div id="pageselector">
+        <center>
+            <ul>
+                <li id="button"><a href="index.php" id="button">HOME</a></li>
+                <li id="button"><a href="html/Underconstruction.php" id="button">ABOUT ME</a></li>
+                <li id="button"><a href="html/Underconstruction.php" id="picture"><img src="src/PIXfb.jpg" alt="Profile" id="picture" padding="none"></a></li>
+                <li id="button"><a href="html/Underconstruction.php" id="button">MANTRA</a></li>
+                <li id="button"><a href="html/Resources.php" id="button">Leaving already?</a></li>
+            </ul>
+        </center>
+        <hr id="header">
+        </div>
+    </div>
+<div class="profile-card">
+    <div class="profile-card-header">
+        <img src="src/ProfilePix.JPG" alt="Profile Image">
+        <h1>Felix Miguel Emberga</h1>
+        <p>CEO, School President, Software Dev</p>
+    </div>
+    <div class="profile-card-body">
+        <p>Hi There! I am Felix. A company CEO at 19 and a school President at the same time. Makes 7 digits a month, and 10 in a year(Gacha Currency). Collector of NFT waifus</div>
+    <div class="profile-card-footer">
+        <a href="html/Validation.php" target="_blank" rel="noopener noreference" >Send Message need kausap</a>
+    </div>
+    <div class="button">
+        <p>I made a calculator to see how rich I am in gacha <br>
+        <a href="html/calculator.php">Open Here</a>
+        </p>
+    </div>
+</div>
+</div>
+</body>
+</html>
